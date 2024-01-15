ce5dfc8dac54a8786bcce6fca666cc78c69463e8
unfoundbug <iam@theadmin.co.uk>
2024-01-15 11:41:06 +0000

    Initial Commit to GitHub

---
+++ Packaged/README.md
@@ -0,0 +1,53 @@
+# Smarter Rail
+
+![Banner Image]()
+
+## Description
+
+Prevents rail stations sending excess packs of items. Also prevents sending partial packs.
+
+## Screenshots
+
+### Manual Install Instructions
+
+Note: If you are playing on Gamepass, your game version is likely behind the steam version. Please check the version compatibility chart below.
+
+Your game folder is likely in one of these places:  
+    • Steam: (A-Z):/steam/steamapps/common/Techtonica  
+    • Gamepass: (A-Z):/XboxGames/Techtonica/Content  
+    • Gamepass: Could also be in C:/Program Data/WindowsApps  
+
+1. Download BepInEx v5.4.21 from [here](https://github.com/BepInEx/BepInEx/releases)
+2. Follow the installation instructions [here](https://docs.bepinex.dev/articles/user_guide/installation/index.html)
+3. Extract the contents of the .zip file for this mod.
+4. Drag the "BepInEx" folder into your game folder.
+5. Change config options. 
+
+## compatibility
+| Mod Version | Game Version |
+| --- | --- |
+| 0.0.3 | 0.2.0 |
+| 0.0.2 | 0.2.0 |
+| 0.0.1 | 0.2.0 |
+
+## Changelog
+
+### V0.0.3
+- Enabled percentage configuration. 
+	- Fixed math error
+- Enabled Inserter Limiting
+	- If a rail would not accept items from another station, it also blocks inserters from adding more items.
+
+### V0.0.2
+- Disabled percentage configuration. 
+	- Doesnt work consistantly enough. Will currently always wait for full packet.
+### V0.0.1
+- Initial Release
+
+## Disclaimer
+
+Note: NEW Games must be loaded, saved, and reloaded for mods to take effect. Existing saves will auto-apply mods. 
+Please be sure to backup your saves before using mods: AppData\LocalLow\Fire Hose Games\Techtonica 
+USE AT YOUR OWN RISK! Techtonica Devs do not provide support for Mods, and cannot recover saves damaged by mod usage.
+
+Some assets may come from Techtonica or from the website created and owned by Fire Hose Games, who hold the copyright of Techtonica. All trademarks and registered trademarks present in any images are proprietary to Fire Hose Games.
---
+++ SmarterRailStations.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+using BepInEx.Configuration;
+using System.Linq;
+
+namespace RightDrag
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class SmarterTransit : BaseUnityPlugin
+    {
+        public const string pluginGuid = "SmarterTransit.nhickling.co.uk";
+        public const string pluginName = "SmarterTransit";
+        public const string pluginVersion = "0.0.0.3";
+        private static BepInEx.Logging.ManualLogSource ModLogger;
+        private static List<int> activatedSlots = new List<int>();
+        public static ConfigEntry<int> EmptySlotsRequired;
+        public static ConfigEntry<int> MinimumLoadPercentage;
+
+
+        public void Awake()
+        {
+            gameObject.hideFlags = HideFlags.HideAndDontSave;
+            Logger.LogInfo("SmarterTransit: started");
+
+            Harmony harmony = new Harmony(pluginGuid);
+            Logger.LogInfo("SmarterTransit: Fetching patch references");
+            {
+                Logger.LogInfo("Patching Outbound");
+                MethodInfo original = AccessTools.Method(typeof(TransitDepotInstance), "StartPacking", new Type[] { });
+                MethodInfo patch = AccessTools.Method(typeof(SmarterTransit), "StartPacking_MyPatch");
+                Logger.LogInfo("SmarterTransit: Starting Patch");
+                harmony.Patch(original, new HarmonyMethod(patch), null);
+                Logger.LogInfo("SmarterTransit: Patched");
+            }
+
+            {
+                Logger.LogInfo("Patching Inbound");
+
+                var methods = AccessTools.GetDeclaredMethods(typeof(TransitDepotInstance));
+                var relevant = methods.First(method => method.Name == "ValidateAddResources" && !method.IsStatic);
+                MethodInfo patch = AccessTools.Method(typeof(SmarterTransit), "ValidateAddResources_MyPatch");
+                Logger.LogInfo("SmarterTransit: Starting Patch");
+                harmony.Patch(relevant, new HarmonyMethod(patch), null);
+                Logger.LogInfo("SmarterTransit: Patched");
+            }
+
+            EmptySlotsRequired = ((BaseUnityPlugin)this).Config.Bind<int>("Config", "EmptySlotsRequired", 50, new ConfigDescription("Number of EMPTY slots required at the destination before a pack can be sent. (Currently Disabled)", (AcceptableValueBase)(object)new AcceptableValueRange<int>(1, 55), Array.Empty<object>()));
+            MinimumLoadPercentage = ((BaseUnityPlugin)this).Config.Bind<int>("Config", "MinimumLoadPercentage", 100, new ConfigDescription("Minimum percentage of items before a pack will be sent. (0 to always send, 100 to only send full carts)", (AcceptableValueBase)(object)new AcceptableValueRange<int>(0, 100), Array.Empty<object>()));
+
+            ModLogger = Logger;
+        }
+
+        public static bool StartPacking_MyPatch(TransitDepotInstance __instance)
+        {
+
+            // If we have a target rail
+            
+            if(__instance.hasPair)
+            {
+                ref TransitDepotInstance pairDepot = ref __instance.pairedDepot.Get();
+                ref Inventory local = ref __instance.GetInputInventory();
+                ref Inventory remote = ref pairDepot.GetOutputInventory();
+                var requiredItems = (float)__instance.maxCapacity;
+
+                int req = MinimumLoadPercentage.Value;
+                int cur = local.GetTotalResourceCount();
+
+                float perc = ((float)cur / requiredItems) * 100;
+                bool isOk = Math.Ceiling(perc) >= req;
+
+
+                if(!isOk)
+                {                    
+                    return false;
+                }
+
+                if (remote.GetNumberOfEmptySlots() < EmptySlotsRequired.Value)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool ValidateAddResources_MyPatch(TransitDepotInstance __instance, ref bool __result, int resId, int inventoryIndex, out int slotNum, AddResourceValidationType validationType)
+        {
+            ref Inventory local = ref __instance.GetInputInventory();
+            slotNum = -1;
+
+            if (local.GetNumberOfEmptySlots() < EmptySlotsRequired.Value)
+            {
+                __result = false;
+                return false;
+            }
+            
+            return true;
+        }
+    }
+}
